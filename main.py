5b484d2b4a3d63a23bf4d0acc87c6939a90e4127
PeterOstr <61982891+PeterOstr@users.noreply.github.com>
2024-01-14 18:58:56 +0300

    requests for predictions done

--- main.py
+++ main.py
@@ -46,10 +46,78 @@ model_xgb = joblib.load('model_xgb.joblib')
 model_nb = joblib.load('model_nb.joblib')
 model_lgbm = joblib.load('model_lgbm.joblib')
 
-@app.post("/model/predict")
+@app.post("/model/predict_xgb")
 async def predict_xgb(data: Dataset_data):
     data = pd.read_json(data.model_dump_json(), orient='list')
     data = data.set_index('index')
     model_prediction = pd.DataFrame(np.round(model_xgb.predict(data)))
     model_prediction = model_prediction.reset_index().to_json()
     return model_prediction
+
+@app.post("/model/predict_catb")
+async def predict_catb(data: Dataset_data):
+    data = pd.read_json(data.model_dump_json(), orient='list')
+    data = data.set_index('index')
+    model_prediction = pd.DataFrame(np.round(model_cat.predict(data)))
+    model_prediction = model_prediction.reset_index().to_json()
+    return model_prediction
+
+@app.post("/model/predict_lgbm")
+async def predict_lgbm(data: Dataset_data):
+    data = pd.read_json(data.model_dump_json(), orient='list')
+    data = data.set_index('index')
+    model_prediction = pd.DataFrame(np.round(model_lgbm.predict(data)))
+    model_prediction = model_prediction.reset_index().to_json()
+    return model_prediction
+
+@app.post("/model/predict_logreg")
+async def predict_logreg(data: Dataset_data):
+    data = pd.read_json(data.model_dump_json(), orient='list')
+    data = data.set_index('index')
+    model_prediction = pd.DataFrame(np.round(model_logreg.predict(data)))
+    model_prediction = model_prediction.reset_index().to_json()
+    return model_prediction
+
+@app.post("/model/predict_knn")
+async def predict_knn(data: Dataset_data):
+    data = pd.read_json(data.model_dump_json(), orient='list')
+    data = data.set_index('index')
+    model_prediction = pd.DataFrame(np.round(model_knn.predict(data)))
+    model_prediction = model_prediction.reset_index().to_json()
+    return model_prediction
+
+
+@app.post("/model/predict_nb")
+async def predict_nb(data: Dataset_data):
+    data = pd.read_json(data.model_dump_json(), orient='list')
+    data = data.set_index('index')
+    model_prediction = pd.DataFrame(np.round(model_nb.predict(data)))
+    model_prediction = model_prediction.reset_index().to_json()
+    return model_prediction
+
+@app.post("/model/predict_ens")
+async def predict_ens(data: Dataset_data):
+    data = pd.read_json(data.model_dump_json(), orient='list')
+    data = data.set_index('index')
+
+    model_prediction_xgb = pd.DataFrame(np.round(model_xgb.predict(data)))
+    model_prediction_catb = pd.DataFrame(np.round(model_cat.predict(data)))
+    model_prediction_lgbm = pd.DataFrame(np.round(model_lgbm.predict(data)))
+    model_prediction_logreg = pd.DataFrame(np.round(model_logreg.predict(data)))
+    model_prediction_nb = pd.DataFrame(np.round(model_nb.predict(data)))
+    model_prediction_knn = pd.DataFrame(np.round(model_knn.predict(data)))
+
+    # averaging predictions
+    ensemble_prediction = (
+                                  model_prediction_xgb +
+                                  model_prediction_catb +
+                                  model_prediction_lgbm +
+                                  model_prediction_logreg +
+                                  model_prediction_nb +
+                                  model_prediction_knn
+                          ) / 6  # for quantity of models
+
+    # to JSON
+    ensemble_prediction_json = ensemble_prediction.reset_index().to_json()
+
+    return ensemble_prediction_json
