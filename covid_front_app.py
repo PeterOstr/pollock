23c5a45fe630b1bf1194ca6262626afd8b7ca08d
PeterOstr <61982891+PeterOstr@users.noreply.github.com>
2024-01-20 02:51:25 +0300

    app request fixed

--- covid_front_app.py
+++ covid_front_app.py
@@ -17,12 +17,12 @@ st.write("""This dashboard will present the info about the COVID-19 dataset, tak
 Kaggle platform (https://www.kaggle.com/datasets/meirnizri/covid19-dataset)).""")
 
 st.markdown("""
- * Use the menu at left to select data and set plot parameters
+ * Use the menu at left to select Charts page or to make predictions
  * Your plots will appear below
 """)
 
 
-# -- Notes on whitening
+# -- Notes on data_set
 with st.expander("See data description"):
     st.markdown("""
  * sex: 1 for female and 2 for male.
@@ -80,16 +80,22 @@ def new_line(n=1):
     for i in range(n):
         st.write("\n")
 
-# File uploader
-uploaded_file = st.sidebar.file_uploader("Upload y_pred.csv file", type=["csv"])
+# Function to plot Confusion Matrix
+def plot_confusion_matrix(cm, model_name):
+    cm_percentage = cm.astype('float') / cm
 
 # Placeholder for dataframes
 y = pd.DataFrame()
 y_scores = pd.DataFrame()
 
-button_answer = st.sidebar.button('Show data')
 
 
+if page == "Info":
+    st.header("""Some info about """)
+
+    st.markdown("""#### ROC Curve (ROC)    """)
+    st.markdown(""" 
+       """)
 
 if page == "Charts":
     st.header("""Charts Demo""")
@@ -101,6 +107,8 @@ if page == "Charts":
     y_pred_lgbm = pd.read_csv('y_pred_lgbm.csv')
     y_pred_logreg = pd.read_csv('y_pred_logreg.csv')
     y_pred_nb = pd.read_csv('y_pred_nb.csv')
+
+    st.markdown("""#### ROC Curve (ROC)    """)
 
     # Чекбоксы для выбора моделей
     use_xgb_model = st.checkbox("Use XGBoost Model", value=False)
@@ -157,196 +165,152 @@ if page == "Charts":
         # Displaying the plot in Streamlit
         st.plotly_chart(fig)
 
-    # if not y_pred.empty:
-    #     st.write('f1_score:', np.round(f1_score(y, y_pred), 3))
-    #     st.write('r2_score:', np.round(r2_score(y_pred, y), 3))
-    #     st.write('balanced_accuracy_score:', np.round(balanced_accuracy_score(y_pred, y), 3))
-    #
-    #     # Calculate ROC AUC
-    #     fpr, tpr, thresholds = roc_curve(y, y_pred)
-    #     roc_auc = roc_auc_score(y, y_pred)
-    #
-    #     # Plot ROC curve
-    #     plt.figure(figsize=(8, 8))
-    #     plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = {:.2f})'.format(roc_auc))
-    #     plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-    #     plt.xlabel('False Positive Rate')
-    #     plt.ylabel('True Positive Rate')
-    #     plt.title('Receiver Operating Characteristic (ROC) Curve')
-    #     plt.legend(loc='lower right')
-    #     st.pyplot(plt)
-    #
-    #     # Plot precision recall
-    #     # calculate precision and recall
-    #     precision, recall, thresholds = precision_recall_curve(y_pred, y)
-    #
-    #     # create precision recall curve
-    #     fig, ax = plt.subplots()
-    #     ax.plot(recall, precision, color='purple')
-    #
-    #     # add axis labels to plot
-    #     ax.set_title('Precision-Recall Curve')
-    #     ax.set_ylabel('Precision')
-    #     ax.set_xlabel('Recall')
-    #
-    #     # display plot
-    #     st.pyplot(plt)
-    #
-    #     # Confusion Matrix
-    #     cm = confusion_matrix(y, y_pred)
-    #
-    #     # Normalize the confusion matrix
-    #     cm_percentage = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    #
-    #     # Display the confusion matrix
-    #     fig, ax = plt.subplots()
-    #     ConfusionMatrixDisplay(confusion_matrix=cm_percentage, display_labels=[0, 1]).plot(cmap='Blues', ax=ax)
-    #     ax.set_title('Confusion Matrix (Normalized)')
-    #     st.pyplot(fig)
-    #
-    # y_pred = pd.read_csv('y_pred.csv')
-    # y = pd.read_csv('y.csv')
-    #
-    # st.write('f1_score:',np.round(f1_score(y, y_pred),3))
-    # st.write('r2_score:',np.round(r2_score(y_pred, y),3))
-    # st.write('balanced_accuracy_score:',np.round(balanced_accuracy_score(y_pred,y),3))
-    #
-    #
-    # # Calculate ROC AUC
-    # fpr, tpr, thresholds = roc_curve(y, y_pred)
-    # roc_auc = roc_auc_score(y, y_pred)
-    #
-    # # Plot ROC curve
-    # plt.figure(figsize=(8, 8))
-    # plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = {:.2f})'.format(roc_auc))
-    # plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-    # plt.xlabel('False Positive Rate')
-    # plt.ylabel('True Positive Rate')
-    # plt.title('Receiver Operating Characteristic (ROC) Curve')
-    # plt.legend(loc='lower right')
-    # st.pyplot(plt)
-    #
-    # # Plot precision recall
-    # #calculate precision and recall
-    # precision, recall, thresholds = precision_recall_curve(y_pred, y)
-    #
-    # #create precision recall curve
-    # fig, ax = plt.subplots()
-    # ax.plot(recall, precision, color='purple')
-    #
-    # #add axis labels to plot
-    # ax.set_title('Precision-Recall Curve')
-    # ax.set_ylabel('Precision')
-    # ax.set_xlabel('Recall')
-    #
-    # #display plot
-    # st.pyplot(plt)
-    #
-    #
-    # # Confusion Matrix
-    # cm = confusion_matrix(y, y_pred)
-    #
-    # # Normalize the confusion matrix
-    # cm_percentage = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    #
-    # # Display the confusion matrix
-    # fig, ax = plt.subplots()
-    # ConfusionMatrixDisplay(confusion_matrix=cm_percentage, display_labels=[0, 1]).plot(cmap='Blues', ax=ax)
-    # ax.set_title('Confusion Matrix (Normalized)')
-    # st.pyplot(fig)
+    st.markdown("""#### Confusion Matrix   """)
+
+    # Button to plot ROC AUC curves
+    if st.button("Plot Confusion Matrix"):
+
+        # Plotting Confusion Matrix for selected models
+        if use_xgb_model:
+            cm = confusion_matrix(y, y_pred_xgb)
+            plot_confusion_matrix(cm, "XGBoost")
+
+        if use_lgbm_model:
+            cm = confusion_matrix(y, y_pred_lgbm)
+            plot_confusion_matrix(cm, "LightGBM")
+
+        if use_catb_model:
+            cm = confusion_matrix(y, y_pred_catb)
+            plot_confusion_matrix(cm, "CatBoost")
+
+        if use_logreg_model:
+            cm = confusion_matrix(y, y_pred_logreg)
+            plot_confusion_matrix(cm, "Logistic Regression")
+
+        if use_nb_model:
+            cm = confusion_matrix(y, y_pred_nb)
+            plot_confusion_matrix(cm, "Naive Bayes")
+
+        if use_knn_model:
+            cm = confusion_matrix(y, y_pred_knn)
+            plot_confusion_matrix(cm, "KNN")
+
+        # # Confusion Matrix
+        # cm = confusion_matrix(y, y_pred_nb)
 
 
+        # # Normalize the confusion matrix
+        # cm_percentage = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+        plot_confusion_matrix(cm, "Confusion Matrix")
+
+        # Display the confusion matrix
+        fig, ax = plt.subplots()
+        ConfusionMatrixDisplay(confusion_matrix=plot_confusion_matrix(), display_labels=[0, 1]).plot(cmap='Blues', ax=ax)
+        ax.set_title('Confusion Matrix (Normalized)')
+        st.pyplot(fig)
 
 
 
 
 if page == "Make prediction":
 
+    uploaded_file = st.file_uploader("Choose a CSV file")
+
     st.header("Select your model and make prediction:")
 
-    # Ваши данные
-    # y = pd.read_csv('y.csv')
-    X = pd.read_csv(uploaded_file)
-    X_dict = X.reset_index().to_dict(orient='list')
+    # # File uploader
+    # uploaded_file = st.sidebar.file_uploader("Upload y_pred.csv file", type=["csv"])
 
-    if button_answer:
+    # Check if file is uploaded
+    if 'uploaded_file' not in st.session_state or st.session_state.uploaded_file is None:
+        st.warning("Please upload a file before making predictions.")
+        # uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
+        st.session_state.uploaded_file = uploaded_file
+
+        button_answer = st.button('Show data')
+
+
+    else:
+
+        X = pd.read_csv(uploaded_file)
+        X_dict = X.reset_index().to_dict(orient='list')
+        st.session_state.uploaded_file = uploaded_file
         st.write(X)
 
-    # Чекбокс для выбора модели
-    use_xgb_model = st.checkbox("Use XGBoost Model", value=False)
-    use_lgbm_model = st.checkbox("Use LightGBM Model", value=False)
-    use_catb_model = st.checkbox("Use CatBoost Model", value=False)
-    use_logreg_model = st.checkbox("Use Logistic Regression Model", value=False)
-    use_knn_model = st.checkbox("Use KNN Model", value=False)
-    use_nb_model = st.checkbox("Use Naive Bayes Model", value=False)
-    use_ens_model = st.checkbox("Use Ensemble Model", value=False)
+        # Чекбокс для выбора модели
+        use_xgb_model = st.checkbox("Use XGBoost Model", value=False)
+        use_lgbm_model = st.checkbox("Use LightGBM Model", value=False)
+        use_catb_model = st.checkbox("Use CatBoost Model", value=False)
+        use_logreg_model = st.checkbox("Use Logistic Regression Model", value=False)
+        use_knn_model = st.checkbox("Use KNN Model", value=False)
+        use_nb_model = st.checkbox("Use Naive Bayes Model", value=False)
+        use_ens_model = st.checkbox("Use Ensemble Model", value=False)
 
-    # Кнопка для отправки запроса
-    if st.button("Make Prediction"):
-        # Отправка запроса в зависимости от выбранной модели
-        if use_xgb_model:
-            endpoint = 'https://covidapp-bmvhwrbbeq-lm.a.run.app/model/predict_xgb'
-        elif use_lgbm_model:
-            endpoint = 'https://covidapp-bmvhwrbbeq-lm.a.run.app/model/predict_lgbm'
-        elif use_catb_model:
-            endpoint = 'https://covidapp-bmvhwrbbeq-lm.a.run.app/model/predict_catb'
-        elif use_logreg_model:
-            endpoint = 'https://covidapp-bmvhwrbbeq-lm.a.run.app/model/predict_logreg'
-        elif use_knn_model:
-            endpoint = 'https://covidapp-bmvhwrbbeq-lm.a.run.app/model/predict_knn'
-        elif use_nb_model:
-            endpoint = 'https://covidapp-bmvhwrbbeq-lm.a.run.app/model/predict_nb'
-        elif use_ens_model:
-            endpoint = 'https://covidapp-bmvhwrbbeq-lm.a.run.app/model/predict_ens'
+        # Кнопка для отправки запроса
+        if st.button("Make Prediction"):
+            # Отправка запроса в зависимости от выбранной модели
+            if use_xgb_model:
+                endpoint = 'https://covidapp-bmvhwrbbeq-lm.a.run.app/model/predict_xgb'
+            elif use_lgbm_model:
+                endpoint = 'https://covidapp-bmvhwrbbeq-lm.a.run.app/model/predict_lgbm'
+            elif use_catb_model:
+                endpoint = 'https://covidapp-bmvhwrbbeq-lm.a.run.app/model/predict_catb'
+            elif use_logreg_model:
+                endpoint = 'https://covidapp-bmvhwrbbeq-lm.a.run.app/model/predict_logreg'
+            elif use_knn_model:
+                endpoint = 'https://covidapp-bmvhwrbbeq-lm.a.run.app/model/predict_knn'
+            elif use_nb_model:
+                endpoint = 'https://covidapp-bmvhwrbbeq-lm.a.run.app/model/predict_nb'
+            elif use_ens_model:
+                endpoint = 'https://covidapp-bmvhwrbbeq-lm.a.run.app/model/predict_ens'
 
-        else:
-            # Добавьте здесь другие модели, если необходимо
-            endpoint = 'https://covidapp-bmvhwrbbeq-lm.a.run.app/model/predict_default'
+            predicted_data_response = requests.post(endpoint, json=X_dict)
+            results_predicted = pd.read_json(StringIO(predicted_data_response.json())).set_index('index')
+            results_predicted.columns = ['Died']
+            st.write('prediction results:')
+            st.write(results_predicted)
 
-        predicted_data_response = requests.post(endpoint, json=X_dict)
-
-        # Проверка успешности запроса
-        if predicted_data_response.status_code == 200:
-            y_scores = pd.read_json(StringIO(predicted_data_response.json())).set_index('index')
-
-            st.dataframe(y_scores)
-
-            #
-            # # Рассчет ROC AUC
-            # fpr, tpr, thresholds = roc_curve(y, y_scores)
-            # roc_auc = roc_auc_score(y, y_scores)
-            #
-            # # Создание графика Plotly
-            # fig = go.Figure()
-            # fig.add_trace(go.Scatter(x=fpr, y=tpr, mode='lines', name='ROC curve'))
-            # fig.add_trace(go.Scatter(x=[0, 1], y=[0, 1], mode='lines', name='Random', line=dict(dash='dash')))
-            # fig.update_layout(
-            #     title='Receiver Operating Characteristic (ROC) Curve',
-            #     xaxis=dict(title='False Positive Rate'),
-            #     yaxis=dict(title='True Positive Rate'),
-            #     showlegend=True
-            # )
-            #
-            # # Отображение графика в Streamlit
-            # st.plotly_chart(fig)
-            # st.write(f'ROC AUC Score: {roc_auc:.2f}')
-            #
-            # # Confusion Matrix
-            # cm = confusion_matrix(y, y_scores)
-            #
-            # # Normalize the confusion matrix
-            # cm_percentage = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-            #
-            # # Display the confusion matrix
-            # fig, ax = plt.subplots()
-            # ConfusionMatrixDisplay(confusion_matrix=cm_percentage, display_labels=[0, 1]).plot(cmap='Blues', ax=ax)
-            # ax.set_title('Confusion Matrix (Normalized)')
-            # st.pyplot(fig)
-
-        else:
-            st.error(f"Error in prediction request. Status code: {predicted_data_response.status_code}")
-
-
+#             # Проверка успешности запроса
+#             if predicted_data_response.status_code == 200:
+#                 y_scores = pd.read_json(StringIO(predicted_data_response.json())).set_index('index')
+#
+#                 st.dataframe(y_scores)
+#
+#                 #
+#                 # # Рассчет ROC AUC
+#                 # fpr, tpr, thresholds = roc_curve(y, y_scores)
+#                 # roc_auc = roc_auc_score(y, y_scores)
+#                 #
+#                 # # Создание графика Plotly
+#                 # fig = go.Figure()
+#                 # fig.add_trace(go.Scatter(x=fpr, y=tpr, mode='lines', name='ROC curve'))
+#                 # fig.add_trace(go.Scatter(x=[0, 1], y=[0, 1], mode='lines', name='Random', line=dict(dash='dash')))
+#                 # fig.update_layout(
+#                 #     title='Receiver Operating Characteristic (ROC) Curve',
+#                 #     xaxis=dict(title='False Positive Rate'),
+#                 #     yaxis=dict(title='True Positive Rate'),
+#                 #     showlegend=True
+#                 # )
+#                 #
+#                 # # Отображение графика в Streamlit
+#                 # st.plotly_chart(fig)
+#                 # st.write(f'ROC AUC Score: {roc_auc:.2f}')
+#                 #
+#                 # # Confusion Matrix
+#                 # cm = confusion_matrix(y, y_scores)
+#                 #
+#                 # # Normalize the confusion matrix
+#                 # cm_percentage = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+#                 #
+#                 # # Display the confusion matrix
+#                 # fig, ax = plt.subplots()
+#                 # ConfusionMatrixDisplay(confusion_matrix=cm_percentage, display_labels=[0, 1]).plot(cmap='Blues', ax=ax)
+#                 # ax.set_title('Confusion Matrix (Normalized)')
+#                 # st.pyplot(fig)
+#
+#             else:
+#                 st.error(f"Error in prediction request. Status code: {predicted_data_response.status_code}")
 
 
 
