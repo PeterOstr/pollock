400cde0fe84be73e612ecd92ba886ef560e4af5b
PeterOstr <61982891+PeterOstr@users.noreply.github.com>
2024-02-18 22:32:49 +0300

    knn model deleted

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class Dataset_data(BaseModel):
 
 
 model_logreg = joblib.load('model_logreg.joblib')
-model_knn = joblib.load('model_knn.joblib')
+# model_knn = joblib.load('model_knn.joblib')
 model_cat = joblib.load('model_catb.joblib')
 model_xgb = joblib.load('model_xgb.joblib')
 model_nb = joblib.load('model_nb.joblib')
@@ -79,13 +79,13 @@ async def predict_logreg(data: Dataset_data):
     model_prediction = model_prediction.reset_index().to_json()
     return model_prediction
 
-@app.post("/model/predict_knn")
-async def predict_knn(data: Dataset_data):
-    data = pd.read_json(data.model_dump_json(), orient='list')
-    data = data.set_index('index')
-    model_prediction = pd.DataFrame(np.round(model_knn.predict(data)))
-    model_prediction = model_prediction.reset_index().to_json()
-    return model_prediction
+# @app.post("/model/predict_knn")
+# async def predict_knn(data: Dataset_data):
+#     data = pd.read_json(data.model_dump_json(), orient='list')
+#     data = data.set_index('index')
+#     model_prediction = pd.DataFrame(np.round(model_knn.predict(data)))
+#     model_prediction = model_prediction.reset_index().to_json()
+#     return model_prediction
 
 
 @app.post("/model/predict_nb")
@@ -106,7 +106,7 @@ async def predict_ens(data: Dataset_data):
     model_prediction_lgbm = pd.DataFrame(np.round(model_lgbm.predict(data)))
     model_prediction_logreg = pd.DataFrame(np.round(model_logreg.predict(data)))
     model_prediction_nb = pd.DataFrame(np.round(model_nb.predict(data)))
-    model_prediction_knn = pd.DataFrame(np.round(model_knn.predict(data)))
+    # model_prediction_knn = pd.DataFrame(np.round(model_knn.predict(data)))
 
     # averaging predictions
     ensemble_prediction = (
@@ -114,9 +114,9 @@ async def predict_ens(data: Dataset_data):
                                   model_prediction_catb +
                                   model_prediction_lgbm +
                                   model_prediction_logreg +
-                                  model_prediction_nb +
-                                  model_prediction_knn
-                          ) / 6  # for quantity of models
+                                  model_prediction_nb
+                                  # model_prediction_knn
+                          ) / 5  # for quantity of models
 
     # to JSON
     ensemble_prediction_json = ensemble_prediction.reset_index().to_json()
